5a27171b64e003b16919023ebd536f7945179379
Dileepa-gif <malaka.18@itfac.mrt.ac.lk>
2022-05-30 23:39:28 +0530

    change date formact

--- configs/app-config.js
+++ configs/app-config.js
@@ -1,21 +1,21 @@
 /*server configurations*/
 
-module.exports = {
-    serverPort: process.env.PORT || 8000,
-    dbHost: "localhost",
-    dbName: "vehicle_service_center",
-    dbPort: "3306",
-    dbUserName: "root",
-    dbPw: ""
-};
-
 // module.exports = {
 //     serverPort: process.env.PORT || 8000,
-//     dbHost: "ebh2y8tqym512wqs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-//     dbName: "zcm9bc7845xszi7q",
+//     dbHost: "localhost",
+//     dbName: "vehicle_service_center",
 //     dbPort: "3306",
-//     dbUserName: "vdq9tm6c61hbwm0z",
-//     dbPw: "ez5sxcixemd3x6m0"
+//     dbUserName: "root",
+//     dbPw: ""
 // };
+
+module.exports = {
+    serverPort: process.env.PORT || 8000,
+    dbHost: "ebh2y8tqym512wqs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    dbName: "zcm9bc7845xszi7q",
+    dbPort: "3306",
+    dbUserName: "vdq9tm6c61hbwm0z",
+    dbPw: "ez5sxcixemd3x6m0"
+};
 
 
